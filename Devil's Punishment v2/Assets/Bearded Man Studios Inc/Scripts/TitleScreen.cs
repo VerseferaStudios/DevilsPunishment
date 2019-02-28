a3ad7808e36a3c6e67ac213f86f974352d54d169
StelloHexis <40399065+StelloHexis@users.noreply.github.com>
2019-02-28 00:47:11 -0400

    Add files via upload

---
+++ Devil's Punishment v2/Assets/Bearded Man Studios Inc/Scripts/TitleScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void singlePlayerButton() {
+
+    }
+    public void multiPlayerButton() {
+
+    }
+    public void optionsButton() {
+
+    }
+    public void creditsButton() {
+
+    }
+    public void exitButton() {
+
+    }
+    public void hatchButton() {
+
+    }
+
+    void Start()
+    {
+        
+    }
+    
+}
