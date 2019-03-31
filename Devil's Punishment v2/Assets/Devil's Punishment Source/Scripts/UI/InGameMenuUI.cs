400884870099b34ce7c1179ccd7559caf91963d6
Anthony <antroe42@gmail.com>
2019-03-31 15:20:41 -0400

    [Anthony] InGameMenu
    Added the in game menu and changed some pill variables

---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/UI/InGameMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenuUI : MonoBehaviour
+{
+    public static InGameMenuUI instance;
+    
+    void Awake() 
+    {
+        instance = this;
+    }
+
+    public void Btn_Continue()
+    {
+        Player.instance.ToggleGameMenu();
+    }
+
+    public void Btn_Options()
+    {
+        Player.instance.ToggleOptionsMenu();
+    }
+
+    public void Btn_Exit()
+    {
+        Player.instance.ExitGame();
+    }
+}
