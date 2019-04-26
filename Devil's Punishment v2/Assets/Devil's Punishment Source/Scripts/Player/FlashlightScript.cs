4c728b7fc6eb7409e085a669de1aad8c8fdf233f
Justin Villamor <38319052+Shallow3@users.noreply.github.com>
2019-04-26 17:14:46 -0400

    Update FlashlightScript.cs
    Moved stuff around

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
@@ -7,11 +7,11 @@ public class FlashlightScript : MonoBehaviour
     [Header("Debug Settings")]
     public KeyCode flashlightButton;//Key to activate flashlight
     public float glowLightIntensity;//Insenity of glowLight light.
-    public float flashLightInsensity;//Insenity of flashLight light.
     public float glowLightRadius;//Size of glowLight
+    public Color glowColor;//GlowLight Color
+    public float flashLightInsensity;//Insenity of flashLight light.
     public float flashLightRange;//Range of Flashlight
     public float flashLightAngle;//Angle of FlashLight(The circle of Light)
-    public Color glowColor;//GlowLight Color
     public Color flashColor;//FlashLight Color
     [Header("Flicker Variables")]
     [SerializeField]
