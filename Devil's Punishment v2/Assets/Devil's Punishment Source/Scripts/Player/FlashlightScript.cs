fb8173dc7baa6f55b13f430c3db7f94ca6662034
Justin Villamor <38319052+Shallow3@users.noreply.github.com>
2019-04-25 20:59:03 -0400

    Flashlight System Revisted
    Flashlight and GlowLigt Added

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
@@ -4,33 +4,70 @@ using UnityEngine;
 
 public class FlashlightScript : MonoBehaviour
 {
+    [Header("Debug Settings")]
+    public KeyCode flashlightButton;//Key to activate flashlight
+    public float glowLightIntensity;//Insenity of glowLight light.
+    public float flashLightInsensity;//Insenity of flashLight light.
+    public float glowLightRadius;//Size of glowLight
+    public float flashLightRange;//Range of Flashlight
+    public float flashLightAngle;//Angle of FlashLight(The circle of Light)
+    public Color glowColor;//GlowLight Color
+    public Color flashColor;//FlashLight Color
     [Header("Flicker Variables")]
     [SerializeField]
     private bool isFlickerEnabled = false;
-
+    [SerializeField]
+    private bool isUpgraded = false;
     [SerializeField]
     private float range = 0.1f;
-
-    private Light lightComponent;
-    private float currentIntensity;
+    private Light glowLight;//GlowLight GameObject
+    private Light flashLight;//FlashLight GameObject
+    private float currentIntensity;//Light Intensity
 
     void Start()
     {
-        lightComponent = GetComponent<Light>();
-        currentIntensity = lightComponent.intensity;
+        glowLight = this.gameObject.transform.Find("Lights/GlowLight").GetComponent<Light>();
+        flashLight = this.gameObject.transform.Find("Lights/Flashlight").GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        //isUpgraded will be coming from the player scripts
+        //isUpgraded == player.isUpgraded();
+
+        if (Input.GetKeyDown(flashlightButton))
         {
-            lightComponent.enabled = !lightComponent.enabled;
+            if(isUpgraded)
+            {
+                flashLight.gameObject.SetActive(!flashLight.gameObject.activeSelf);
+                flashLight.intensity = flashLightInsensity;
+                flashLight.range = flashLightRange;
+                flashLight.spotAngle = flashLightAngle;
+                flashLight.color = flashColor;
+                flashLight.enabled = !flashLight.enabled;
+            }
+            else
+            {
+                glowLight.gameObject.SetActive(!glowLight.gameObject.activeSelf);
+                glowLight.intensity = glowLightIntensity;
+                glowLight.range = glowLightRadius;
+                glowLight.color = glowColor;
+                glowLight.enabled = !glowLight.enabled;
+            }
+                
         }
 
         if (isFlickerEnabled)
         {
-            lightComponent.intensity = Random.Range(currentIntensity - range, currentIntensity + range);
+            if(isUpgraded)
+            {
+                flashLight.intensity = Random.Range(currentIntensity - range, currentIntensity + range);
+            }
+            else
+            {
+                glowLight.intensity = Random.Range(currentIntensity - range, currentIntensity + range);
+            }
         }
     }
 }
