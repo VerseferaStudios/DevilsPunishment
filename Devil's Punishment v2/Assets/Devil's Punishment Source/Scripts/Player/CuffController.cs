185e0b3bd5acb2d4a9381197f6b094f5ffdd8a79
dkbird <davidkbird@outlook.com>
2019-05-19 15:56:38 -0400

    Cuff Finished
    Finishing up cuff mechanic

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/CuffController.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/CuffController.cs
@@ -1,6 +1,6 @@
 ﻿//Author: Dave Bird
 //Date: Sunday, May 12, 2019
-    //Last Edited: Sunday, May 14, 2019
+    //Last Edited: Sunday, May 19, 2019
         //By: Dave Bird
             //Purpose: Write the script
  //Written For: Devil's Punishment v2
@@ -15,76 +15,40 @@ using UnityEngine.UI;
 public class CuffController : MonoBehaviour
 {
     public static CuffController instance;
-    public PlayerController playercontroller; 
+    public PlayerController playercontroller;
     public bool isCuffed;
     public bool pisLocked; //Pistol
     public bool risLocked; //Assault Rifle
     public bool sisLocked; //Shotgun
-    public bool pAisLocked; //Pistol Ammo
-    public bool rAisLocked; //Assault Rifle Ammo
-    public bool sAisLocked;  // Shotgun Ammo
-    public bool medkitisLocked; //Medic Kit
-    public bool pillisLocked;   //Pills
-    public bool gPart1isLocked;  //Generator Part A
-    public bool gPart2isLocked;  //Generator Part B
-    public bool gPart3isLocked;  //Generator Part C
-    public bool flUpgradeisLocked;  //Flashlight Upgrade
-    public bool inventoryhasSpace; 
 
 
     void Awake()
     {
-        isCuffed = true;
-        inventoryhasSpace = true;
+        Cuff();
     }
 
 
     //Can that item get picked up?
-    public void Cuffed()
+    public void Cuff()
     {
-        if (isCuffed == true)
-        {
-            pisLocked = true;
-            risLocked = true;
-            sisLocked = true;
-            pAisLocked = false;
-            rAisLocked = false;
-            sAisLocked = false;
-            medkitisLocked = false;
-            pillisLocked = false;
-            gPart1isLocked = false;
-            gPart2isLocked = false;
-            gPart3isLocked = false;
-            flUpgradeisLocked = false;
-
-        }
-        else if (!isCuffed)
-        {
-            pisLocked = false;
-            risLocked = false;
-            sisLocked = false;
-            pAisLocked = false;
-            rAisLocked = false;
-            sAisLocked = false;
-            medkitisLocked = false;
-            pillisLocked = false;
-            gPart1isLocked = false;
-            gPart2isLocked = false;
-            gPart3isLocked = false;
-            flUpgradeisLocked = false;
-        }
+        isCuffed = true;
+        pisLocked = true;
+        risLocked = true;
+        sisLocked = true;
     }
 
-    //public void OnTriggerEnter(Collider Ladder)
-    //{
-    //    if (tag.player == true && isCuffed == true && Input.GetKeyDown(KeyCode.E))
-    //    {
-    //        playercontroller.VerticalLocomotion();
-    //    }
-    //    else if (tag.player == true && !isCuffed && Input.GetKeyDown(KeyCode.E))
-    //    {
-    //        playercontroller.VerticalLocomotion();
-    //    }
-    //}
+    public void Uncuff()
+    {
+        isCuffed = false;
+        pisLocked = false;
+        risLocked = false;
+        sisLocked = false;
+    }
+    public void OnTriggerEnter(Collider Ladder)
+    {
+        if (tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            playercontroller.VerticalLocomotion();
+        }
+    }
 }
-
--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/InteractBehaviour.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/InteractBehaviour.cs
@@ -14,7 +14,7 @@ public class InteractBehaviour : MonoBehaviour
     public Image interactablePromptBackground;
     public TextMeshProUGUI interactablePrompt;
 
-    private IInteractable focusedInteractable;
+    private InteractableLoot focusedInteractable;
     private bool interactableInVicinity = false;
 
     private float interactMaxTime;
@@ -50,11 +50,11 @@ public class InteractBehaviour : MonoBehaviour
             
             float maxDot = -1.0f;
 
-            IInteractable interactableToFocus = null;
+            InteractableLoot interactableToFocus = null;
             
             foreach (Collider col in hitColliders) {
 
-                IInteractable nearbyInteractable = col.GetComponent<IInteractable>();
+                InteractableLoot nearbyInteractable = col.GetComponent<InteractableLoot>(); //Changed Interactable loot from IInteracable after the statement. Sajomis 5/19/2019
                 if(nearbyInteractable != null) {
 
                     Vector3 pickupToCam = col.transform.position - Camera.main.transform.position;
@@ -117,9 +117,22 @@ public class InteractBehaviour : MonoBehaviour
 
         progressBarImage.fillAmount = (interactElapsedTime/interactMaxTime);
 
-        if(interactElapsedTime >= interactMaxTime) {
-            focusedInteractable.OnInteract();
-            InteractableUnFocus();
+
+        //Edited on 5/19/2019 by Sajomis to modify pick up to being blocked by handcuffs.
+        if(interactElapsedTime >= interactMaxTime)
+        {
+            CuffController cuffcontroller = gameObject.GetComponent<CuffController>();
+            if (!cuffcontroller.isCuffed)
+            {
+                focusedInteractable.OnInteract();
+                InteractableUnFocus();
+            }
+            else
+            {
+                if (!(focusedInteractable.item is GunItem))
+                focusedInteractable.OnInteract(); //What is a focused interactable, how do we specify a gun?
+                InteractableUnFocus();
+            }
         }
 
         if (Input.GetButton("Interact"))
