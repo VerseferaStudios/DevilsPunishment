2d152426afbe07958d5c5a180f143eb8a9ddd2f0
Shubham <chronorealminteractive@gmail.com>
2019-05-24 04:25:01 +0530

    Implemented room spawning
    Rooms now spawn. Next step: tracer logic.

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Level Generation/RoomSpawn.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Level Generation/RoomSpawn.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class RoomSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] RoomSpawns;
+    public GameObject[] StaticRooms;
+    public GameObject[] RandoRooms;
+    public float[] RandoRoomIndices;
+    public int NumOfRooms;
+    private int randomiser, prevrando=0;
+
     void Start()
     {
-        
-    }
+        NumOfRooms = Random.Range(20, 25);
+        for (int i = 0; i <= 5; i++)
+        {
+            randomiser = Random.Range(0, RoomSpawns.Length);
+            if (prevrando != randomiser)
+                GameObject.Instantiate(StaticRooms[i], RoomSpawns[randomiser].position, RoomSpawns[randomiser].rotation);
+            else i--;
+            prevrando = randomiser;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
