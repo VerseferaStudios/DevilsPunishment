8f1fb82b4e840a9aefce60206259ac65a339516b
Shubham <chronorealminteractive@gmail.com>
2019-05-24 12:23:13 +0530

    Added a check to make sure 2 rooms don't spawn in the same place

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Level Generation/RoomSpawn.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Level Generation/RoomSpawn.cs
@@ -9,20 +9,41 @@ public class RoomSpawn : MonoBehaviour
     public GameObject[] RandoRooms;
     public float[] RandoRoomIndices;
     public int NumOfRooms;
-    private int randomiser, prevrando=0;
+    private int randomiser;
+    public int[] TakenCells;
+    private bool Overlap = false;
 
     void Start()
     {
+        int i = 0;
         NumOfRooms = Random.Range(20, 25);
-        for (int i = 0; i <= 5; i++)
+        while (i<5)
         {
-            randomiser = Random.Range(0, RoomSpawns.Length);
+            randomiser = Random.Range(0, RoomSpawns.Length-1);
+            for (int s = 0; s < TakenCells.Length-1; s++)
+            {
+                if (TakenCells[s] == randomiser)
+                    Overlap = true;
+            }
+            if (!Overlap)
+            {
+                GameObject.Instantiate(StaticRooms[i], RoomSpawns[randomiser].position, RoomSpawns[randomiser].rotation);
+                i++;
+                TakenCells[i] = randomiser;
+            }
+            Overlap = false;
+            
+        }
+        NumOfRooms -= 5;
+        /*for (int i = 0; i <= 5; i++)
+        {
+            randomiser = Random.Range(0, RoomSpawns.Length - 1);
             if (prevrando != randomiser)
                 GameObject.Instantiate(StaticRooms[i], RoomSpawns[randomiser].position, RoomSpawns[randomiser].rotation);
             else i--;
             prevrando = randomiser;
-        }
+        }*/
 
-        
+
     }
 }
