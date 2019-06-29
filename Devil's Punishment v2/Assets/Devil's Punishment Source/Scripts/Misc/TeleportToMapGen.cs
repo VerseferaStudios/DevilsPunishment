9f1310092c7f345ef2cd247ad442ac7bfc3bd30c
JoAMD <joasus12345@gmail.com>
2019-06-29 10:58:35 +0300

    Added cube script to teleport, made ground bigger

---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Misc/TeleportToMapGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToMapGen : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger...");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("teleporting...");
+            Vector3 playerPos = other.transform.position;
+            other.transform.position = new Vector3(playerPos.x - 220, playerPos.y, playerPos.z);
+            other.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+}
