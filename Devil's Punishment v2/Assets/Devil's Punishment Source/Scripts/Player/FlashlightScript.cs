fe6f84fc1f2d8530eaecc9e9b50698473ecfa0d7
dkbird <davidkbird@outlook.com>
2019-07-09 23:39:49 -0400

    Have fixed the upgrade issue in the inventory, and adjusted the logic in the flashlight path.

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Player/FlashlightScript.cs
@@ -21,6 +21,7 @@ public class FlashlightScript : MonoBehaviour
     public bool dark;
     public bool isUpgraded;
     public int count;
+    public int countno;
 
     [Header("Player controlling LSD angle")]
     [Tooltip("Spotlight GameObject goes here.")]
@@ -36,6 +37,24 @@ public class FlashlightScript : MonoBehaviour
         isUpgraded = false;
         dark = false;
         count = 0;
+    }
+
+    public void ToggleUpgraded()
+    {
+        if (glow.enabled && !beam.enabled)
+        {
+            glow.enabled = false;
+            beam.enabled = true;
+        }
+        else if (!glow.enabled && beam.enabled)
+        {
+            glow.enabled = beam.enabled = false;
+        }
+        else
+        {
+            glow.enabled = true;
+            beam.enabled = false;
+        }
     }
 
     public void Upgraded()
@@ -57,60 +76,95 @@ public class FlashlightScript : MonoBehaviour
         //    Debug.Log("You have set the upgrade to being true temporarily.");
         //}
         //////////////////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.G) && isUpgraded == false && count == 0)
+        
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            glow.enabled = false;
-            dark = true;
-            count++;
-            Debug.Log("You have covered the glowstick.");
-        }
-        else if (Input.GetKeyDown(KeyCode.G) && !isUpgraded && dark && count == 1)
-        {
-            glow.enabled = true;
-            dark = false;
-            count--;
-            Debug.Log("You have uncovered the glowstick.");
+            if (isUpgraded)
+            {
+                flashLight.SetActive(true);
+                ToggleUpgraded();
+            }
+            else
+            {
+                if (countno == 0)
+                {
+                    glow.enabled = false;
+                    dark = true;
+                    countno++;
+                    Debug.Log("You have covered the glowstick.");
+                }
+                else if (countno == 1)
+                {
+                    glow.enabled = true;
+                    dark = false;
+                    countno--;
+                    Debug.Log("You have uncovered the glowstick.");
+                }
+            }
         }
 
 
-        //Glow stick to flashlight
-        if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 0)
-        {
-            glow.enabled = false;
-            beam.enabled = true;
-            Debug.Log("You have covered your glowstick and turned on your flashlight.");
-            count++;
-            Debug.Log("Count now set to 1.");
-        }
-        //Flashlight to glowstick
-        else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G)  && count == 1)
-        {
-            glow.enabled = true;
-            beam.enabled = false;
-            Debug.Log("You have uncovered your glowstick.");
-            count++;
-            Debug.Log("Count now set to 2.");
-        }
-        //Glowstick to darkness
-        else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 2)
-        {
-            glow.enabled = false;
-            beam.enabled = false;
-            Debug.Log("You have plunged yourself in complete darkness.");
-            dark = true;
-            count++;
-            Debug.Log("Count now set to 3.");
-        }
-        //Darkness to glowstick
-        else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 3)
-        {
-            glow.enabled = true;
-            beam.enabled = false;
-            Debug.Log("You have uncovered your glowstick.");
-            count = 0; //This represents ONLY glowstick is glowing <0 = glowstick, 1 = flashlight, 2 = all off>
-            Debug.Log("Count is reset to 0.");
-            dark = false;
-        }
+
+
+
+
+
+
+
+        //if (Input.GetKeyDown(KeyCode.G) && isUpgraded == false && count == 0)
+        //{
+        //    glow.enabled = false;
+        //    dark = true;
+        //    count++;
+        //    Debug.Log("You have covered the glowstick.");
+        //}
+        //else if (Input.GetKeyDown(KeyCode.G) && !isUpgraded && dark && count == 1)
+        //{
+        //    glow.enabled = true;
+        //    dark = false;
+        //    count--;
+        //    Debug.Log("You have uncovered the glowstick.");
+        //}
+
+
+        ////Glow stick to flashlight
+        //if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 0)
+        //{
+        //    glow.enabled = false;
+        //    beam.enabled = true;
+        //    Debug.Log("You have covered your glowstick and turned on your flashlight.");
+        //    count++;
+        //    Debug.Log("Count now set to 1.");
+        //}
+        ////Flashlight to glowstick
+        //else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G)  && count == 1)
+        //{
+        //    glow.enabled = true;
+        //    beam.enabled = false;
+        //    Debug.Log("You have uncovered your glowstick.");
+        //    count++;
+        //    Debug.Log("Count now set to 2.");
+        //}
+        ////Glowstick to darkness
+        //else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 2)
+        //{
+        //    glow.enabled = false;
+        //    beam.enabled = false;
+        //    Debug.Log("You have plunged yourself in complete darkness.");
+        //    dark = true;
+        //    count++;
+        //    Debug.Log("Count now set to 3.");
+        //}
+        ////Darkness to glowstick
+        //else if (isUpgraded == true && Input.GetKeyDown(KeyCode.G) && count == 3)
+        //{
+        //    glow.enabled = true;
+        //    beam.enabled = false;
+        //    Debug.Log("You have uncovered your glowstick.");
+        //    count = 0; //This represents ONLY glowstick is glowing <0 = glowstick, 1 = flashlight, 2 = all off>
+        //    Debug.Log("Count is reset to 0.");
+        //    dark = false;
+        //}
         ////Making it so that the player can move the light.
         //if (Input.GetAxis("Mouse ScrollWheel") && isUpgraded)
         //{
