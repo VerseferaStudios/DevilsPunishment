85b8f40a9f51e2097649fea748d023b551cf7d73
dkbird <davidkbird@outlook.com>
2019-07-14 19:04:15 -0400

    added 8 new animations dealing with the flashlights ability, changed my scene slightly for this current task, added a new script 'lsdlightadjustments' to handle the new animations. hope this works for you all... note the hotkeys are set to arrow keys and q

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Items/LSDLightAdjustments.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Items/LSDLightAdjustments.cs
@@ -1,8 +1,8 @@
 ﻿//Author: Dave Bird
 //Date: Saturday, July 13, 2019
-//Last Edited: Saturday, July 13, 2019
-//By: Dave Bird
-//Purpose: This script handles the four abilities of the flashlight.
+    //Last Edited: Saturday, July 14, 2019
+        //By: Dave Bird
+            //Purpose: Finish the script.
 //Written For: Devil's Punishment v2
 //Purpose: This script details all things that has to do with the limitations of the cuffed mechanic.
 //Notes: Further information can be found at http://www.nolocationyet.com (Dead link)
@@ -11,35 +11,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LSDLightAdjustments : MonoBehaviour
 {
     public Light beam; //"Flashlight" on player model under Player > Player Model > GlowStick/Flashlight GameObject > FlashlightCase - EmptyGameObject > Flashlight
-    [Header("Rotation of the Light")]
-    public float rotSpeed = 1.0f;
-    public float homeRotx = 4.03f;
-    public float homeRoty = -33.412f;
-    public float homeRotz = 1.97f;
-    public float maxRotx = 0.0f;
-    public float maxRoty = 0.0f;
-    public float maxRotz = 0.0f;
-    public float minRotx = -10.0f;
-    public float minRoty = -10.0f;
-    public float minRotz = -10.0f;
+    public GameObject flashlightCase;
+    public Animation[] clips; 
 
-    public float curRotx;
-    public float curRoty;
-    public float curRotz;
+    public float homeAngle = 45.0f;
+    [SerializeField]
+    private bool isRaised = false;
+    [SerializeField]
+    private bool isCenter = true;
+    [SerializeField]
+    private bool isLowered = false;
+    [SerializeField]
+    private bool isWide = false;
+    [SerializeField]
+    private bool isNarrow = false;
+    [SerializeField]
+    private bool isNormal = true;
 
-    [Header("Spot Angle of Light")]
-    public float turnSpeed = 1.0f;
-    public float homeAngle = 45f;
-    public float maxAngle = 65f;
-    public float minAngle = 25f;
+    public float homeX, homeY, homeZ = 0;
 
     public void Awake()
     {
         beam = GetComponent<Light>();
+        beam.GetComponent<Light>().range = 30;
+        beam.GetComponent<Light>().spotAngle = 45;
+    }
+
+    /*Possible States 
+     * Center / Normal
+     * Center / Wide
+     * Center / Narrow
+     * Raised / Normal
+     * Raised / Wide
+     * Raised / Narrow
+     * Lowered / Normal
+     * Lowered / Wide
+     * Lowered / Narrow 
+     */
+
+    void SetToNormal()
+    {
+        isCenter = true;
+        isRaised = false;
+        isLowered = false;
+        isWide = false;
+        isNarrow = false;
+        isNormal = true;
     }
 
     // Update is called once per frame
@@ -48,66 +70,108 @@ public class LSDLightAdjustments : MonoBehaviour
         //Raising the light.
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-
-            Debug.Log("Player has raised the light up.");
-            if (curRotx >= 10.0f)
+            if (isCenter)
             {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("CenterUp");
+                isRaised = true;
+                isCenter = false;
+                Debug.Log("Player has raised the light up to the ceiling");
             }
-            if (curRoty >= 10.0f)
+            else if (isLowered)
             {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("DownUp");
+                isCenter = true;
+                isLowered = false;
+                Debug.Log("Player has raised the light from the floor.");
             }
-            if (curRotz >= 10.0f)
-            {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
-            }
-
         }
         //Lowering the light.
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Player has lowered the light down.");
-            if (curRotx <= -10.0f)
+            if (isCenter)
             {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("CenterDown");
+                isLowered = true;
+                isCenter = false;
+                Debug.Log("Player has lowered the light down to the floor");
             }
-            if (curRoty <= -10.0f)
+            else if (isRaised)
             {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
-            }
-            if (curRotz <= -10.0f)
-            {
-                //If it exceeds the maximum maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("UpCenter");
+                isRaised = false;
+                isCenter = true;
+                Debug.Log("Player has lowered the light down to eye level.");
             }
         }
-
-        //Widening the beam.
+        //Widening the light.
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            beam.spotAngle = 62; //Must slowly expand the angle to maximum of 62
-            Debug.Log("Player has expanded the lights view.");
-            if (maxAngle >= 62)
+            if (isNormal)
             {
-                //If the angles maximum value exceeds maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("CenterWiden");
+                isNormal = false;
+                isWide = true;
+                Debug.Log("Player has made the beam wider.");
+            }
+            else if (isNarrow)
+            {
+                flashlightCase.GetComponent<Animation>().Play("NarrowCenter");
+                isNormal = true;
+                isNarrow = false;
+                Debug.Log("Player has lowered the light down to eye level.");
             }
         }
-        //Narrowing the beam.
+        //Narrowing the light.
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            beam.spotAngle = 22; //Must slowly expand the angle to maximum of 22
-            Debug.Log("Player has expanded the lights view.");
-            if (maxAngle <= 22)
+            if (isNormal)
             {
-                //If the angles maximum value exceeds maxvalue then put it at x.xxf
+                flashlightCase.GetComponent<Animation>().Play("CenterNarrow");
+                isNormal = false;
+                isNarrow = true;
+                Debug.Log("Player has made the beam wider.");
+            }
+            else if (isWide)
+            {
+                flashlightCase.GetComponent<Animation>().Play("WidenCenter");
+                isNormal = true;
+                isWide = false;
+                Debug.Log("Player has made the beam back to normal.");
             }
         }
         //Snapping the light back to its home position.
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            //Snap the Rotation back to its normal rotation on all 3 x,y,z and back to its normal angle of 42
-            beam.transform.Rotate(homeRotx, homeRoty, homeRotz);
+            if (isRaised && isWide)
+            {
+                flashlightCase.GetComponent<Animation>().Play("UpCenter");
+                flashlightCase.GetComponent<Animation>().Play("WidenCenter");
+            }
+            else if (isRaised && isNarrow)
+            {
+                flashlightCase.GetComponent<Animation>().Play("UpCenter");
+                flashlightCase.GetComponent<Animation>().Play("NarrowCenter");
+            }
+            else if (isLowered && isWide)
+            {
+                flashlightCase.GetComponent<Animation>().Play("DownCenter");
+                flashlightCase.GetComponent<Animation>().Play("WidenCenter");
+            }
+            else if (isLowered && isNarrow)
+            {
+                flashlightCase.GetComponent<Animation>().Play("DownCenter");
+                flashlightCase.GetComponent<Animation>().Play("NarrowCenter");
+            }
+            else if (isCenter && isWide)
+            {
+                flashlightCase.GetComponent<Animation>().Play("WideCenter");
+            }
+            else if (isCenter && isNarrow)
+            {
+                flashlightCase.GetComponent<Animation>().Play("NarrowCenter");
+            }
             beam.spotAngle = homeAngle;
+            SetToNormal();
             Debug.Log("Player has reset the location of the light source");
         }
     }
