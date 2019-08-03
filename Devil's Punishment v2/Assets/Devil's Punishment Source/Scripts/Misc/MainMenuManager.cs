1ae5d9534e2307fae5ac9c9923c29588fdb7a604
dkbird <davidkbird@outlook.com>
2019-08-03 16:16:25 -0400

    small changes to main menu working on settings

---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Misc/MainMenuManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button singlePlayer;
+    public Button multiPlayer;
+    public Button credits;
+    public Button reportBug;
+    public Button settings;
+    public Button quit;
+    public Button setEx;
+    public Button repEx;
+
+    public GameObject reportPanel;
+    public GameObject settingsCanvas;
+
+    public int[] scenes;
+    public Cursor pointer;
+       
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        singlePlayer.GetComponent<Button>().enabled = true;
+        multiPlayer.GetComponent<Button>().enabled = true;
+        credits.GetComponent<Button>().enabled = true;
+        reportBug.GetComponent<Button>().enabled = true;
+        settings.GetComponent<Button>().enabled = true;
+        quit.GetComponent<Button>().enabled = true;
+        reportPanel.SetActive(false);
+        settingsCanvas.SetActive(false);
+    }
+
+    public void SingleSart()
+    {
+        SceneManager.LoadScene(scenes[1]);
+    }
+
+    public void MultiStart()
+    {
+        SceneManager.LoadScene(scenes[2]);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(scenes[3]);
+    }
+
+    public void ReportBug()
+    {
+        reportPanel.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        settingsCanvas.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReportExit()
+    {
+        reportPanel.SetActive(false);
+    }
+
+    public void SettingsExit()
+    {
+        settingsCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Misc/SettingsManager.cs
@@ -0,0 +1,61 @@
+﻿//Author: David Bird
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public Dropdown resolutionOptions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionOptions.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResOption = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResOption = i;
+            }
+        }
+        resolutionOptions.AddOptions(options);
+        resolutionOptions.value = currentResOption;
+        resolutionOptions.RefreshShownValue();
+    }
+
+    public void QualitySetter(int qualitySet)
+    {
+        QualitySettings.SetQualityLevel(qualitySet);
+    }
+
+    public void FullScreen(bool FullScreen)
+    {
+        Screen.fullScreen = FullScreen;
+    }
+
+    public void ResolutionPick(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
