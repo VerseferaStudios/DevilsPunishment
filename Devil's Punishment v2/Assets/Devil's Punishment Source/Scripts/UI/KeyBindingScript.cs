6b697dec8862b6fd12e478b8721f4a79f413860b
dkbird <davidkbird@outlook.com>
2019-08-06 00:08:09 -0400

    working on keybinding script, saving my work here in case of catastrophic failure

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/UI/KeyBindingScript.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/UI/KeyBindingScript.cs
@@ -2,9 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 
 public class KeyBindingScript : MonoBehaviour
 {
+    Transform menuImage;
+    Event keyEvent;
+    Text buttonText;
+    public KeyCode newKey;
 
+    private bool waitingForKey;
+
+    public void Start()
+    {
+        menuImage = this.transform;
+        //menuImage = this.transform.Find("Image.ButtonListContent").transform;
+        //menuImage = transform.FindChild("Image.ButtonListContent");
+        waitingForKey = false;
+
+        for(int i = 0; i < 17; i++)
+        {
+            if(menuImage.GetChild(i).name == "Button.Forward")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.forward.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Back")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.backward.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Left")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.left.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Right")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.right.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Crouch")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.crouch.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Sprint")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.sprint.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Shoot")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.shoot.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Reload")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.reload.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightToggle")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightToggle.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightNarrow")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightNarrow.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightWiden")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightWiden.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightUp")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightUp.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightDown")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightDown.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.FlashlightHome")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.flashlightCenter.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Pause")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.pause.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Inventory")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.inventory.ToString();
+            }
+            else if (menuImage.GetChild(i).name == "Button.Interact")
+            {
+                menuImage.GetChild(i).GetComponentInChildren<Text>().text = GameManager.gm.interact.ToString();
+            }
+        }
+    }
 }
---
+++ Devil's Punishment v2/Assets/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    public KeyCode forward { get; set; } //defined 1
+    public KeyCode backward { get; set; } //defined 1
+    public KeyCode left { get; set; } //defined 1
+    public KeyCode right { get; set; } //defined 1
+    public KeyCode interact { get; set; } //defined 1
+    public KeyCode crouch { get; set; } //defined 1
+    public KeyCode sprint { get; set; } //defined 1
+    public KeyCode shoot { get; set; } //defined 1
+    public KeyCode reload { get; set; } //defined 1
+    public KeyCode flashlightToggle { get; set; } //defined 1
+    public KeyCode flashlightNarrow { get; set; } //defined 1
+    public KeyCode flashlightWiden { get; set; } //defined 1
+    public KeyCode flashlightUp { get; set; } //defined 1
+    public KeyCode flashlightDown { get; set; } //defined 1
+    public KeyCode flashlightCenter { get; set; } //defined 1
+    public KeyCode pause { get; set; } //defined 1
+    public KeyCode inventory { get; set; } //defined 1
+
+
+    public void Awake()
+    {
+        if (gm == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gm = this;
+        }
+        else if (gm != this)
+        {
+            Destroy(gameObject);
+        }
+        forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Vertical", "W"));
+        backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Vertical", "S"));
+        left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Horizontal", "A"));
+        right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Horizontal", "D"));
+        crouch = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "LeftControl"));
+        sprint = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift"));
+        interact = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E"));
+        reload = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Reload", "R"));
+        shoot = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Fire1", "Mouse0")); 
+        flashlightToggle = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("toggleflashlight", "G"));
+        flashlightNarrow = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Flashlight Narrow", "RightArrow"));
+        flashlightWiden = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Flashlight Wide", "LeftArrow"));
+        flashlightUp = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Flashlight Up", "UpArrow"));
+        flashlightDown = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Flashlight Down", "DownArrow"));
+        flashlightCenter = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Flashlight Home", "Q"));
+        inventory = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Open Inventory", "Tab"));
+        pause = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Cancel", "Escape"));
+    }
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
