80a81a669e0531a2b764fd9b2dd9baeedda70a3d
JoAMD <joasus12345@gmail.com>
2019-08-12 15:25:01 +0530

    Tried to add Interactable loot to vent cover, not working as of now

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/World/InteractableLoot.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/World/InteractableLoot.cs
@@ -8,6 +8,7 @@ public class InteractableLoot : MonoBehaviour, IInteractable
     public Item item;
     public int stock;
     public float timeToPickUp = .5f;
+    public bool isDoor = false;
     public string Prompt() {
         return "Pick up " + item.name + " (" + stock + ")";
     }
@@ -21,14 +22,29 @@ public class InteractableLoot : MonoBehaviour, IInteractable
     }
 
     public void OnInteract() {
-        Debug.Log("Picked up " + item.name + " x" + stock + ".");
-		gameObject.SetActive(false);
-		Inventory.instance.AddItem(item, stock);
-		if (gameObject.name.Contains("(Clone)"))
-		{
-			Destroy(gameObject);
-		}
+        if (isDoor)
+        {
+            StartCoroutine(OpenVentCover());
+        }
+        else
+        {
+            Debug.Log("Picked up " + item.name + " x" + stock + ".");
+            gameObject.SetActive(false);
+            Inventory.instance.AddItem(item, stock);
+            if (gameObject.name.Contains("(Clone)"))
+            {
+                Destroy(gameObject);
+            }
+        }
 	}
 
+    private IEnumerator OpenVentCover()
+    {
+        while (transform.localEulerAngles.z >= 89) //get sth better
+        {
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, transform.localEulerAngles + new Vector3(0, 0, 90), Time.deltaTime);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
 }
