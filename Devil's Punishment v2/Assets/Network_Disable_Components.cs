31cae05b65f42519459b5f3a7f5314b0af62fa31
Klauensprung <36606771+Klauensprung@users.noreply.github.com>
2019-08-16 00:51:20 +0200

    Multiplayer push
    MP Functionality added

---
+++ Devil's Punishment v2/Assets/Network_Disable_Components.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Network_Disable_Components : NetworkBehaviour
+{
+
+    [SerializeField]
+    public List<Behaviour> objects_to_disable = new List<Behaviour>();
+    public List<GameObject> UIThings = new List<GameObject>();
+    public GameObject Third_Person_Model;
+
+    // Disables components that should not be visible to other players (like UI and stats)
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour behaviour in objects_to_disable)
+            {
+                behaviour.enabled = false;
+            }
+
+            foreach (GameObject behaviour in UIThings)
+            {
+                behaviour.SetActive(false);
+
+            }
+        }
+        else
+        {
+
+            Third_Person_Model.SetActive(false);
+        }
+
+    }
+
+
+}
