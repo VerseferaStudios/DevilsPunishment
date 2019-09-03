6e3a41c6b57851a3fb53f11ae75675d7efdb7f57
dkbird <davidkbird@outlook.com>
2019-09-03 19:49:53 -0400

    Adding the script

--- Devil's Punishment v2/Assets/LaserCutter.cs
+++ Devil's Punishment v2/Assets/LaserCutter.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class LaserCutter : MonoBehaviour
 {
+    public Player health;
+    private int healthStart;
+    private float timeToRelease = 15;
+    public Player cuffed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("StartZappin");
+        health.GetComponent<Health>().curHealth = healthStart;
     }
+
+    IEnumerator StartZappin()
+    {
+        //as long as the players health doesnt drop below players health at the start and the interact button is being pressed
+        while (Input.GetKeyDown("Interact"))
+        {
+            timeToRelease -= Time.deltaTime;
+            if (timeToRelease <= 0)
+            {
+                cuffed.GetComponent<CuffController>().isCuffed = false;
+                Debug.Log("Cuffs have been removed");
+            }
+            else if (Input.GetKeyUp("Interact"))
+            {
+                timeToRelease = 15f;
+            }
+        }
+        yield return null;
+    }
+
+    //public void OnTriggerEnter(Collider enemy)
+    //{
+    //    if ()
+    //    {
+    //        return;
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
