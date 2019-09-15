45fbff141d84464280ab38eecc7187d0201453dc
JoAMD <joasus12345@gmail.com>
2019-09-15 18:07:03 +0530

    Edits to door and vent cover

--- Devil's Punishment v2/Assets/MapGen/Scripts/Misc/InteractableDoor.cs
+++ Devil's Punishment v2/Assets/MapGen/Scripts/Misc/InteractableDoor.cs
@@ -12,7 +12,7 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
     public GameObject brokenFloorCollidors;
 
-    private int l = 0;
+    //private int l = 0;
 
     public string Prompt()
     {
@@ -31,11 +31,11 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
     public void OnInteract()
     {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+
         if (isVentCover)
         {
             Debug.Log("Opening vent cover");
-            gameObject.GetComponent<BoxCollider>().enabled = false;
-
             Transform t = transform.parent.parent.parent.GetChild(2).GetChild(0); //WILL WORK ON CORRIDOR ONLY!!! TAKE ROOM SEPARATE
             Instantiate(brokenFloorCollidors, t.position, t.rotation, t.parent);
             t.gameObject.SetActive(false);
@@ -46,7 +46,6 @@ public class InteractableDoor : MonoBehaviour, IInteractable
         else
         {
             Debug.Log("Opening door");
-            gameObject.GetComponent<BoxCollider>().enabled = false;
             StartCoroutine(OpenDoor());
         }
 
@@ -66,15 +65,16 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
     private IEnumerator OpenVentCover()
     {
-        
+        float t = 0;
 
         Transform parentGb = transform.parent;
-        while (transform.parent.localEulerAngles.z <= 80)
+        while (transform.parent.localEulerAngles.z < 90 && t < 1.1f)
         {
-
-
-            parentGb.localEulerAngles += Vector3.Lerp(parentGb.localEulerAngles, parentGb.localEulerAngles + new Vector3(0, 0, 90), Time.deltaTime);
+            parentGb.localEulerAngles = new Vector3(parentGb.localEulerAngles.x, parentGb.localEulerAngles.y, Mathf.Lerp(0, 90, t));
             //gameObject.SetActive(false);
+            Debug.Log("t = " + t);
+            t += Time.deltaTime;
+            yield return new WaitForSeconds(0.1f);
         }
 
         yield return new WaitForSeconds(0.5f);
@@ -86,19 +86,19 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
         Transform door0 = transform.parent.GetChild(1);
         Transform door1 = transform.parent.GetChild(2);
-        while (t < .99f && door0.localScale.z > 0.01f && door1.localScale.z > 0.01f)// && l < 1000)
+        while (t < 1.1f)// && door0.localScale.z > 0.01f && door1.localScale.z > 0.01f)// && l < 1000)
         {
-            door0.localScale = new Vector3(1, 1, Mathf.Lerp(door0.localScale.z, 0, t));
-            //door0.localPosition = new Vector3(door0.localPosition.x, door0.localPosition.y, Mathf.Lerp(door0.localPosition.z, door0.localPosition.z - 0.5f, t));
+            //door0.localScale = new Vector3(1, 1, Mathf.Lerp(door0.localScale.z, 0, t));
+            door0.localPosition = new Vector3(door0.localPosition.x, door0.localPosition.y, Mathf.Lerp(-0.5f, -0.5f - 0.5f, t));
 
-            door1.localScale = new Vector3(1, 1, Mathf.Lerp(door1.localScale.z, 0, t));
-            //door1.localPosition = new Vector3(door1.localPosition.x, door1.localPosition.y, Mathf.Lerp(door1.localPosition.z, door1.localPosition.z + 0.5f, t));
+            //door1.localScale = new Vector3(1, 1, Mathf.Lerp(door1.localScale.z, 0, t));
+            door1.localPosition = new Vector3(door1.localPosition.x, door1.localPosition.y, Mathf.Lerp(0.5f, 0.5f + 0.5f, t));
 
             //door0.localScale = Vector3.Lerp(door0.localScale, door0.localScale - new Vector3(0, 0, 0.1f), Time.deltaTime * 100);
             //door1.localScale = Vector3.Lerp(door1.localScale, door1.localScale - new Vector3(0, 0, 0.1f), Time.deltaTime * 10000);
-            ++l;
-            t += Time.deltaTime * 0.1f;
-            Debug.Log("t = " + t);
+            //++l;
+            t += Time.deltaTime * 0.9f;
+            //Debug.Log("t = " + t);
             yield return new WaitForSeconds(0.1f);
         }
     }
