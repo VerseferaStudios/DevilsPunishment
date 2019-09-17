7141b68ea8cc0aa9306f34bbe2c3725e2f783db6
Klauensprung <36606771+Klauensprung@users.noreply.github.com>
2019-09-17 06:14:28 +0200

    Multiplayer push with chat system

---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Drug/Chat/Network_Chat.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Network_Chat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<TextMeshProUGUI> chatMessages = new List<TextMeshProUGUI>();
+    public GameObject message;
+    public bool chat_enabled = true;
+    public float currentHeight = 0;
+    public float messageDistance;
+    public int messages = 0;
+
+    public void ActivateChat()
+    {
+        if (chat_enabled)
+        {
+
+   
+            chatMessages.Add(message.GetComponent<TextMeshProUGUI>());
+            messageDistance = message.GetComponent<RectTransform>().sizeDelta.y;
+            currentHeight = transform.position.y; 
+
+            messages = 2;
+            addMessage("Hello");
+            addMessage("World!");
+        }
+    }
+
+    public void addMessage(string text, string username)
+    {
+        GameObject newMsg = Instantiate(message, transform);
+        newMsg.GetComponent<TextMeshProUGUI>().SetText(string.Format("[{0}] {1} : {2} ", System.DateTime.Now.ToShortTimeString(), username, text));
+        newMsg.transform.position = newMsg.transform.position + new Vector3(0, messageDistance * messages, 0);
+        currentHeight = newMsg.transform.position.y;
+        chatMessages.Add(message.GetComponent<TextMeshProUGUI>());
+
+        messages++;
+
+
+
+
+    }
+    public void addMessage(string text)
+    {
+        GameObject newMsg = Instantiate(message, transform);
+        newMsg.GetComponent<TextMeshProUGUI>().SetText(string.Format("[{0}] {1}", System.DateTime.Now.ToShortTimeString(), text));
+        newMsg.transform.position = newMsg.transform.position + new Vector3(0, messageDistance * messages, 0);
+        currentHeight = newMsg.transform.position.y;
+        chatMessages.Add(message.GetComponent<TextMeshProUGUI>());
+
+        messages++;
+    }
+}
---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Drug/Chat/sendMessage_GUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class sendMessage_GUI : MonoBehaviour
+{
+    TMP_InputField tmp;
+ 
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TMP_InputField>();
+    }
+
+    public void sendMessage(BaseEventData e)
+    {
+        
+        NetworkManager_Drug.instance.sendMessage(tmp.text, SystemInfo.deviceName);
+        tmp.text = "";
+
+    }
+
+
+}
