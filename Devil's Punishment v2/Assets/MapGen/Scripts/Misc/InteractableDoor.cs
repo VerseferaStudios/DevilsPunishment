098ec8494c59e03a28b787a1861fd326b3ca1f42
JoAMD <joasus12345@gmail.com>
2019-09-27 22:50:14 +0530

    Smoothed out lerping of door and vent cover

--- Devil's Punishment v2/Assets/MapGen/Scripts/Misc/InteractableDoor.cs
+++ Devil's Punishment v2/Assets/MapGen/Scripts/Misc/InteractableDoor.cs
@@ -73,8 +73,8 @@ public class InteractableDoor : MonoBehaviour, IInteractable
             parentGb.localEulerAngles = new Vector3(parentGb.localEulerAngles.x, parentGb.localEulerAngles.y, Mathf.Lerp(0, 90, t));
             //gameObject.SetActive(false);
             Debug.Log("t = " + t);
-            t += Time.deltaTime;
-            yield return new WaitForSeconds(0.1f);
+            t += Time.deltaTime * 0.4f;
+            yield return new WaitForSeconds(0.01f);
         }
 
         yield return new WaitForSeconds(0.5f);
@@ -86,20 +86,30 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
         Transform door0 = transform.parent.GetChild(1);
         Transform door1 = transform.parent.GetChild(2);
+
+        Vector3 velocity = Vector3.zero;
+
         while (t < 1.1f)// && door0.localScale.z > 0.01f && door1.localScale.z > 0.01f)// && l < 1000)
         {
-            //door0.localScale = new Vector3(1, 1, Mathf.Lerp(door0.localScale.z, 0, t));
-            door0.localPosition = new Vector3(door0.localPosition.x, door0.localPosition.y, Mathf.Lerp(-0.5f, -0.5f - 0.5f, t));
 
-            //door1.localScale = new Vector3(1, 1, Mathf.Lerp(door1.localScale.z, 0, t));
+            //door0.localPosition = Vector3.Lerp(door0.localPosition, new Vector3(door0.localPosition.x, door0.localPosition.y, door0.localPosition.z - 0.5f), t);
+            //door1.localPosition = Vector3.Lerp(door1.localPosition, new Vector3(door1.localPosition.x, door1.localPosition.y, door1.localPosition.z + 0.5f), t);
+
+            //door0.localPosition = Vector3.SmoothDamp(door0.localPosition)
+
+            door0.localPosition = new Vector3(door0.localPosition.x, door0.localPosition.y, Mathf.Lerp(-0.5f, -0.5f - 0.5f, t));
             door1.localPosition = new Vector3(door1.localPosition.x, door1.localPosition.y, Mathf.Lerp(0.5f, 0.5f + 0.5f, t));
+            
+            //door0.localScale = new Vector3(1, 1, Mathf.Lerp(door0.localScale.z, 0, t));
+            //door1.localScale = new Vector3(1, 1, Mathf.Lerp(door1.localScale.z, 0, t));
 
             //door0.localScale = Vector3.Lerp(door0.localScale, door0.localScale - new Vector3(0, 0, 0.1f), Time.deltaTime * 100);
             //door1.localScale = Vector3.Lerp(door1.localScale, door1.localScale - new Vector3(0, 0, 0.1f), Time.deltaTime * 10000);
             //++l;
-            t += Time.deltaTime * 0.9f;
+            //t += Time.deltaTime * 0.04f; //For Vector3.Lerp
+            t += Time.deltaTime * 0.4f; //For Mathf.Lerp
             //Debug.Log("t = " + t);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.01f);
         }
     }
 
