fcbfc2b7e071338b54abeb24ea02c4ab815f8855
JoAMD <joasus12345@gmail.com>
2019-10-20 19:49:09 +0530

    Starting new work

---
+++ Devil's Punishment v2/Assets/MapGen/Scripts/CorridorTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorTest : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        string otherTag = other.transform.parent.tag;
+        if (otherTag.Equals("CorridorI") || otherTag.Equals("CorridorL") || otherTag.Equals("CorridorT") || otherTag.Equals("CorridorX"))
+        {
+            if(!(transform.parent.tag.Equals("CorridorI") && otherTag.Equals("CorridorI")))
+            {
+
+            }
+            Data.instance.collidedCorridors.Add(gameObject);
+            ////Debug.Log(Data.instance.collisionCount + "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        }
+    }
+
+}
