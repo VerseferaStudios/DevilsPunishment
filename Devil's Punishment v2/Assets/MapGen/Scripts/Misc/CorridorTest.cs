a8a83b3d69db4004e574ba8c8225a71c5fc358cd
JoAMD <joasus12345@gmail.com>
2019-10-20 21:21:22 +0530

    Added bug fix for un connected rooms in map gen (Removed rooms easily)

--- Devil's Punishment v2/Assets/MapGen/Scripts/Misc/CorridorTest.cs
+++ Devil's Punishment v2/Assets/MapGen/Scripts/Misc/CorridorTest.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class CorridorTest : MonoBehaviour
 {
+    public GameObject prevRoom;
+    [SerializeField]
+    private bool isCollided = false;
+
     private void Start()
     {
-
+        StartCoroutine(RoomRemove());
     }
     private void OnTriggerEnter(Collider other)
     {
         string otherTag = other.transform.parent.tag;
         if (otherTag.Equals("CorridorI") || otherTag.Equals("CorridorL") || otherTag.Equals("CorridorT") || otherTag.Equals("CorridorX"))
         {
-            if(!(transform.parent.tag.Equals("CorridorI") && otherTag.Equals("CorridorI")))
-            {
+            isCollided = true;
+        }
+    }
 
-            }
-            Data.instance.collidedCorridors.Add(gameObject);
-            ////Debug.Log(Data.instance.collisionCount + "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    private IEnumerator RoomRemove()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!isCollided)
+        {
+            prevRoom.SetActive(false);
+            Debug.Log("Room Removed!");
+            gameObject.SetActive(false);
         }
     }
 
