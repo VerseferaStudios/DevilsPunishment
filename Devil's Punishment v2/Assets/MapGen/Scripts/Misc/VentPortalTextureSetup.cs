8b04c3c8b2221153b632dfbcf2b6ce23c06c4e87
JoAMD <joasus12345@gmail.com>
2019-12-15 22:06:11 +0530

    WIP fixing recently found broken vent portal

--- Devil's Punishment v2/Assets/MapGen/Scripts/Misc/VentPortalTextureSetup.cs
+++ Devil's Punishment v2/Assets/MapGen/Scripts/Misc/VentPortalTextureSetup.cs
@@ -6,10 +6,37 @@ public class VentPortalTextureSetup : MonoBehaviour
 {
     public Camera ventCam;
     public Material ventCamMat;
+    public GameObject ventCoverHolder;
+    private MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
+        meshRenderer = GetComponent<MeshRenderer>();
         StartCoroutine(WaitForPlayerJoin());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!ventCoverHolder.activeSelf && other.tag.StartsWith("Player"))
+        {
+            ventCam = other.transform.GetChild(1).GetChild(1).GetComponent<Camera>();
+            if (ventCam.targetTexture != null)
+            {
+                ventCam.targetTexture.Release();
+            }
+
+            ventCam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            ventCamMat.mainTexture = ventCam.targetTexture;
+            meshRenderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.StartsWith("Player"))
+        {
+            meshRenderer.enabled = false;
+        }
     }
 
     private IEnumerator WaitForPlayerJoin()
