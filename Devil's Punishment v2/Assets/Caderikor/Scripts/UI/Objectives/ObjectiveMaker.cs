5684c2f1fb8934fd4cc7a8c047a3af5fffa89fa5
caderikor <wesley@stormace.net>
2019-12-19 10:47:51 +0100

    Inventory update missions
    Inventory system and mission description added
    
    Keys are w and s for up and down and enter for choose mission and leave it

---
+++ Devil's Punishment v2/Assets/Caderikor/Scripts/UI/Objectives/ObjectiveMaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objective", menuName = "Mission maker/Objective")]
+public class ObjectiveMaker : ScriptableObject
+{
+    public string missionName;
+    public bool complete;
+    public bool isSelected;
+    
+    [TextArea]
+    public string description;
+ 
+    ///<summary>
+    ///Use 2 keys in the valueList key 0 = current value
+    ///Use key 1 for max value
+    ///</summary>
+    public Dictionary<string, int[]> valueList = new Dictionary<string, int[]>();
+}
---
+++ Devil's Punishment v2/Assets/Caderikor/Scripts/UI/Objectives/ObjectiveManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public List<ObjectiveMaker> objectives = new List<ObjectiveMaker>();
+    public List<GameObject> missionObjectList = new List<GameObject>();
+    public GameObject missionUIPrefab;
+    public Transform missionBoxListBox;
+
+    public GameObject ObjectiveGameObject;
+    public GameObject missionGameObject;
+
+    public bool inMission = false;
+    private int missionIndex = 0;
+
+    private void Awake()
+    {
+        missionGameObject.SetActive(false);
+
+        for (int i = 0; i < objectives.Count; i++)
+        {
+                GameObject missionPrefab = Instantiate(missionUIPrefab, missionBoxListBox);
+                missionPrefab.GetComponent<Text>().text = objectives[i].missionName;
+                missionObjectList.Add(missionPrefab);
+                missionObjectList[i].GetComponent<MissionBox>().mission = Instantiate(objectives[i]);
+        }
+
+        string oldName = missionObjectList[0].GetComponent<Text>().text;
+        string newString = ">" + oldName + "<";
+        missionObjectList[0].GetComponent<Text>().text = newString;
+        missionObjectList[0].GetComponent<MissionBox>().mission.isSelected = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ChooseMission();
+        }
+    }
+
+    public void UpdateList(int index)
+    {
+        for (int i = 0; i < missionObjectList.Count; i++)
+        {
+            if (missionObjectList[i].GetComponent<MissionBox>().mission.complete)
+            {
+                missionObjectList[i].GetComponentInChildren<Image>().color = Color.green;
+            }
+        }
+    }
+
+    public void UpdateList(ObjectiveMaker _Mission)
+    {
+        GameObject missionPrefab = Instantiate(missionUIPrefab, missionBoxListBox);
+        missionObjectList.Add(missionUIPrefab);
+        missionPrefab.GetComponent<Text>().text = _Mission.name;
+    }
+
+    public void MoveUp()
+    {
+        for (int i = 0; i < missionObjectList.Count; i++)
+        {
+            if (missionObjectList[i].GetComponent<MissionBox>().mission.isSelected && i-1 > -1)
+            {
+                missionObjectList[i].GetComponent<Text>().text = missionObjectList[i].GetComponent<MissionBox>().mission.missionName;
+                string oldName = missionObjectList[i-1].GetComponent<Text>().text;
+                string newString = ">" + oldName + "<";
+                missionObjectList[i-1].GetComponent<Text>().text = newString;
+                missionObjectList[i].GetComponent<MissionBox>().mission.isSelected = false;
+                missionObjectList[i-1].GetComponent<MissionBox>().mission.isSelected = true;
+                missionIndex = i-1;
+                return;
+            }
+        }
+    }
+
+    public void MoveDown()
+    {
+        for (int i = 0; i < missionObjectList.Count; i++)
+        {
+            if (missionObjectList[i].GetComponent<MissionBox>().mission.isSelected && i+1 < missionObjectList.Count)
+            {
+                missionObjectList[i].GetComponent<Text>().text = missionObjectList[i].GetComponent<MissionBox>().mission.missionName;
+                string oldName = missionObjectList[i+1].GetComponent<Text>().text;
+                string newString = ">" + oldName + "<";
+                missionObjectList[i+1].GetComponent<Text>().text = newString;
+                missionObjectList[i].GetComponent<MissionBox>().mission.isSelected = false;
+                missionObjectList[i+1].GetComponent<MissionBox>().mission.isSelected = true;
+                missionIndex = i+1;
+                return;
+            }
+        }
+    }
+
+    public void ChooseMission()
+    {
+        inMission = !inMission;
+
+        if (inMission)
+        {
+            ObjectiveGameObject.SetActive(false);
+            missionGameObject.SetActive(true);
+
+            var componentsInMission = missionGameObject.GetComponentsInChildren<Text>();
+            foreach (Text obj in componentsInMission)
+            {
+                if (obj.name == "Mission name Text") { obj.text = missionObjectList[missionIndex].GetComponent<MissionBox>().mission.missionName; }
+                if (obj.name == "MissionList box") { obj.text = missionObjectList[missionIndex].GetComponent<MissionBox>().mission.description; }
+            }
+        }
+        else 
+        {
+            ObjectiveGameObject.SetActive(true);
+            missionGameObject.SetActive(false);
+        }
+    }
+
+}
