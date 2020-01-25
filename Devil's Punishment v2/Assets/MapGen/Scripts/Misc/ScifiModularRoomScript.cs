040c1076f830fd113f9d795531d2cc25aebf93d2
JoAMD <joasus12345@gmail.com>
2020-01-26 02:19:49 +0530

    Tried tiling of textures in i corridor (keeping for later); starting scifimodular room generation; added texture tiling for modular room wall too

---
+++ Devil's Punishment v2/Assets/MapGen/Scripts/Misc/ScifiModularRoomScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScifiModularRoomScript : MonoBehaviour
+{
+
+    public GameObject ceiling;
+    public GameObject floor;
+    public GameObject wall;
+    public GameObject wallWithDoor;
+    public GameObject scifiDoor;
+    public GameObject ventCover;
+
+    //Room sizes according to floor tiles; So, 1 x 1 is 6.275 x 6.275
+    private int x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FloornCeiling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FloornCeiling()
+    {
+        x = Random.Range(1, 7);
+        y = Random.Range(1, 7);
+
+
+
+    }
+
+}
