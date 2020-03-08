7439ec99f559387738eec178975af802f17d18e4
Neroid <Neroid@users.noreply.github.com>
2020-03-08 19:14:49 +0200

    Ui-Objectives-Version-1.0
    Implemented a new prefab : player_revamped_temp which includes my changes.

---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Data/NeroidTempFolder/InventoryTabScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InventoryTabScript : MonoBehaviour
+{
+    private int curTab;
+    public GameObject IntentoryBut;
+    public GameObject ObjectivesBut;
+    public GameObject Objs;
+    public GameObject Invs;
+    void Start()
+    {
+        curTab = 0;
+    }
+    public void ChangeTab() // change to inventory or objectives
+    {
+        if (curTab == 0)
+        {
+            curTab = 1;
+            IntentoryBut.SetActive(false);
+            Invs.SetActive(false);
+            ObjectivesBut.SetActive(true);
+            Objs.SetActive(true);
+ 
+        }
+        else
+        {
+            curTab = 0;
+            ObjectivesBut.SetActive(false);
+            Objs.SetActive(false);
+            IntentoryBut.SetActive(true);
+            Invs.SetActive(true);
+        } 
+    }
+}
---
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Data/NeroidTempFolder/ObjectivesHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivesHandler : MonoBehaviour
+{
+    Dictionary<int,GameObject> objectives;
+    Dictionary<int, GameObject> objectivesLights;
+    List<Transform> SortList;
+    void Start()
+    {
+        objectives = new Dictionary<int,GameObject>();
+        objectivesLights = new Dictionary<int, GameObject>();
+        SortList = new List<Transform>();
+        foreach (Transform child in gameObject.transform)
+        {
+            SortList.Add(child);
+        }
+        int i = 0;
+        foreach(Transform child in SortList)
+        {
+            foreach(Transform inchild in SortList)
+            {
+                if (inchild.GetSiblingIndex() == i)
+                {
+                    objectivesLights.Add(i, inchild.GetChild(0).gameObject);
+                    objectives.Add(i, inchild.gameObject);
+                    break;
+                }
+            }
+            i++;
+        }
+        foreach(KeyValuePair<int,GameObject> obj in objectives)
+        {
+            Debug.Log(obj.Key);
+        }
+    }
+
+    public void FinishObjective(int obj)
+    {
+        if (objectivesLights.ContainsKey(obj) && ! objectivesLights[obj].activeSelf)
+        {
+            objectivesLights[obj].SetActive(true);
+        }
+    }
+    public void RevealObjective(int obj)
+    {
+        if (objectives.ContainsKey(obj) && !objectives[obj].activeSelf)
+        {
+            objectives[obj].SetActive(true);
+        }
+    }
+}
