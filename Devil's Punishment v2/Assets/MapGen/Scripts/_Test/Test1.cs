4b69e3d81d931035b4ec665523389c41c5b54abe
JoAMD <joasus12345@gmail.com>
2020-06-28 01:19:03 +0530

    some test files and mirror thing

---
+++ Devil's Punishment v2/Assets/MapGen/Scripts/_Test/Test1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : Test
+{
+    //private void Start()
+    //{
+    //    List<int> ar= new List<int>();
+    //    ar.Add(1);
+    //    Debug.Log(ar.Capacity);
+    //}
+    protected override void DebugCall()
+    {
+        Debug.Log("Test1 Script");
+    }
+}
---
+++ Devil's Punishment v2/Assets/MapGen/Scripts/_Test/Test2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : Test
+{
+    protected override void DebugCall()
+    {
+        Debug.Log("Test2 Script");
+    }
+}
