f88e819191ecebe46a4c71c6c471c6cbf110467f
JoAMD <joasus12345@gmail.com>
2020-07-14 04:52:58 +0530

    THird person stuff heavily wip

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Server_DoorSpawner.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Server_DoorSpawner.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
 public class Server_DoorSpawner : NetworkBehaviour
 {
+    //temp
+
+    public List<AnimationClip> animClips;
+    public List<Object> otherObjects;
+
+    public AnimatorOverrideController animatorOController;
+
+
     public static Server_DoorSpawner instance;
     private void Awake()
     {
@@ -28,3 +41,52 @@ public class Server_DoorSpawner : NetworkBehaviour
     }
 
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(Server_DoorSpawner))]
+public class EditorScriptAnimation : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Server_DoorSpawner server_DoorSpawner = target as Server_DoorSpawner;
+
+        if(GUILayout.Button("Copy Animations From Selection Into AOC"))
+        {
+            Object[] selectedObjects = Selection.objects;
+            server_DoorSpawner.animClips = new List<AnimationClip>();
+            server_DoorSpawner.otherObjects = new List<Object>();
+
+
+            for (int i = 0; i < selectedObjects.Length; i++)
+            {
+                List<KeyValuePair<AnimationClip, AnimationClip>> overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+                Debug.Log("count = " + overrides.Count);
+                server_DoorSpawner.animatorOController.GetOverrides(overrides);
+                Debug.Log("count = " + overrides.Count);
+                if (selectedObjects[i] is AnimationClip)
+                {
+                    server_DoorSpawner.animClips.Add(selectedObjects[i] as AnimationClip);
+
+                    for (int i = 0; i < server_DoorSpawner.animatorOController.overridesCount; i++)
+                    {
+
+                    }
+
+                    Debug.Log("server_DoorSpawner.animatorOController.overridesCount) " + server_DoorSpawner.animatorOController.overridesCount);
+                }
+                else
+                {
+                    server_DoorSpawner.otherObjects.Add(selectedObjects[i]);
+                    Debug.LogWarning("not selected an anim clip but its a -> " + selectedObjects[i].GetType());
+                }
+            }
+        }
+
+    }
+}
+
+#endif
