cf16a7d0a1c30ddc41367e69bac1574fa9885d87
JoAMD <joasus12345@gmail.com>
2020-07-27 22:56:28 +0530

    Working on some unit tests

---
+++ Devil's Punishment v2/Assets/Tests/GunTestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class GunTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GunTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GunTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+
+            SceneManager.LoadScene("NetworkItemTest");
+
+            GameObject networkGb = GameObject.Find("NETWORK");
+
+            Assert.NotNull(networkGb);
+
+            yield return null;
+        }
+    }
+}
