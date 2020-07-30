5ef4e277f00253fc9c2fe3036ad31eec4a8ac4c1
JoAMD <joasus12345@gmail.com>
2020-07-30 18:48:29 +0530

    Copied anims out of fbx and saved the values from previous anim clips, and added to aoc as well

--- Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Server_DoorSpawner.cs
+++ Devil's Punishment v2/Assets/Devil's Punishment Source/Scripts/Network/Server_DoorSpawner.cs
@@ -17,8 +17,8 @@ public class Server_DoorSpawner : NetworkBehaviour
     public List<Object> otherObjects;
     public GameObject parentFbx;
 
-    //public AnimatorOverrideController animatorOController;
-    //public List<KeyValuePair<AnimationClip, AnimationClip>> overrides;
+    public AnimatorOverrideController animatorOController;
+    public List<KeyValuePair<AnimationClip, AnimationClip>> overrides;
 #endif
 
     public static Server_DoorSpawner instance;
@@ -59,92 +59,102 @@ public class EditorScriptAnimation : Editor
 
         if(GUILayout.Button("Copy Animations From Selection Into AOC"))
         {
+
+
+
+
+
+            Object[] selectedObjects = Selection.objects;
+            server_DoorSpawner.animClips = new List<AnimationClip>();
             server_DoorSpawner.otherObjects = new List<Object>();
-            string copyFromName;
-            bool isSuccess = false;
+            AnimationClip curr;
 
-            ModelImporter modelImporter = new ModelImporter();
-            //modelImporter.importAnimation =  
-
-            for (int q = 0; q < server_DoorSpawner.copyFromAnimClips.Count; q++)
+            for (int i = 0; i < selectedObjects.Length; i++)
             {
-                copyFromName = server_DoorSpawner.copyFromAnimClips[q].name;
-                for (int e = 0; e < server_DoorSpawner.animClips.Count; e++)
+                server_DoorSpawner.overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>(server_DoorSpawner.animatorOController.overridesCount);
+                server_DoorSpawner.animatorOController.GetOverrides(server_DoorSpawner.overrides);
+                //Debug.Log("count = " + overrides.Count);
+                ////AnimationClipPair[] overrideAnimationClips = server_DoorSpawner.animatorOController.clips;
+                //Debug.Log("count = " + overrides.Count);
+                if (selectedObjects[i] is AnimationClip)
                 {
-                    AnimationClipSettings animationClipSettings = AnimationUtility.GetAnimationClipSettings(server_DoorSpawner.copyFromAnimClips[q]);
-                    if (server_DoorSpawner.animClips[e].name.Equals(copyFromName))
+                    curr = selectedObjects[i] as AnimationClip;
+                    server_DoorSpawner.animClips.Add(curr);
+                    Debug.Log("curr name = " + curr.name);
+
+                    Debug.Log("count = " + server_DoorSpawner.animatorOController.overridesCount);
+                    Debug.Log("count = " + server_DoorSpawner.overrides.Count);
+                    for (int r = 0; r < server_DoorSpawner.animatorOController.overridesCount; r++)
                     {
-                        AnimationUtility.SetAnimationClipSettings(server_DoorSpawner.animClips[e], animationClipSettings);
-                        //Debug.Log("wrap mode of " + server_DoorSpawner.copyFromAnimClips[q].name + " = " + server_DoorSpawner.copyFromAnimClips[q].wrapMode);
-                        //server_DoorSpawner.animClips[e].wrapMode = server_DoorSpawner.copyFromAnimClips[q].wrapMode;
-                        //server_DoorSpawner.animClips[e].frameRate = server_DoorSpawner.copyFromAnimClips[q].frameRate;
-                        //AnimationEvent[] animEvents = server_DoorSpawner.copyFromAnimClips[q].events;
-                        //for (int t = 0; t < animEvents.Length; t++)
-                        //{
-                        //    server_DoorSpawner.animClips[e].AddEvent(animEvents[t]);
-                        //}
-                        //server_DoorSpawner.animClips[e].localBounds = server_DoorSpawner.copyFromAnimClips[q].localBounds;
-
-                        //EditorUtility.SetDirty(server_DoorSpawner.parentFbx);
-
-                        //EditorUtility.SetDirty(server_DoorSpawner.animClips[e]);
-                        //EditorUtility.SetDirty(server_DoorSpawner.parentFbx);
-                        isSuccess = true;
-                        break;
+                        //Debug.Log(server_DoorSpawner.overrides[r].Key + " && " + server_DoorSpawner.overrides[r].Value);
+                        if (server_DoorSpawner.overrides[r].Key.ToString().StartsWith(curr.name))
+                        {
+                            Debug.Log("g");
+                            server_DoorSpawner.overrides[r] = new KeyValuePair<AnimationClip, AnimationClip>(server_DoorSpawner.overrides[r].Key, curr);
+                        }
                     }
+
+                    server_DoorSpawner.animatorOController.ApplyOverrides(server_DoorSpawner.overrides);
+
+                    Debug.Log("server_DoorSpawner.animatorOController.overridesCount) " + server_DoorSpawner.animatorOController.overridesCount);
                 }
-                if (!isSuccess)
+                else
                 {
-                    server_DoorSpawner.otherObjects.Add(server_DoorSpawner.copyFromAnimClips[q]);
+                    server_DoorSpawner.otherObjects.Add(selectedObjects[i]);
+                    Debug.LogWarning("not selected an anim clip but its a -> " + selectedObjects[i].GetType());
                 }
-                isSuccess = false;
             }
+
+
+
+
+
+            //server_DoorSpawner.otherObjects = new List<Object>();
+            //string copyFromName;
+            //bool isSuccess = false;
+
+            ////ModelImporter modelImporter = new ModelImporter();
+            ////modelImporter.importAnimation =  
+
+            //for (int q = 0; q < server_DoorSpawner.copyFromAnimClips.Count; q++)
+            //{
+            //    copyFromName = server_DoorSpawner.copyFromAnimClips[q].name;
+            //    for (int e = 0; e < server_DoorSpawner.animClips.Count; e++)
+            //    {
+            //        AnimationClipSettings animationClipSettings = AnimationUtility.GetAnimationClipSettings(server_DoorSpawner.copyFromAnimClips[q]);
+            //        if (server_DoorSpawner.animClips[e].name.Equals(copyFromName))
+            //        {
+            //            AnimationUtility.SetAnimationClipSettings(server_DoorSpawner.animClips[e], animationClipSettings);
+            //            //Debug.Log("wrap mode of " + server_DoorSpawner.copyFromAnimClips[q].name + " = " + server_DoorSpawner.copyFromAnimClips[q].wrapMode);
+            //            //server_DoorSpawner.animClips[e].wrapMode = server_DoorSpawner.copyFromAnimClips[q].wrapMode;
+            //            //server_DoorSpawner.animClips[e].frameRate = server_DoorSpawner.copyFromAnimClips[q].frameRate;
+            //            //AnimationEvent[] animEvents = server_DoorSpawner.copyFromAnimClips[q].events;
+            //            //for (int t = 0; t < animEvents.Length; t++)
+            //            //{
+            //            //    server_DoorSpawner.animClips[e].AddEvent(animEvents[t]);
+            //            //}
+            //            //server_DoorSpawner.animClips[e].localBounds = server_DoorSpawner.copyFromAnimClips[q].localBounds;
+
+            //            //EditorUtility.SetDirty(server_DoorSpawner.parentFbx);
+
+            //            //EditorUtility.SetDirty(server_DoorSpawner.animClips[e]);
+            //            //EditorUtility.SetDirty(server_DoorSpawner.parentFbx);
+            //            isSuccess = true;
+            //            break;
+            //        }
+            //    }
+            //    if (!isSuccess)
+            //    {
+            //        server_DoorSpawner.otherObjects.Add(server_DoorSpawner.copyFromAnimClips[q]);
+            //    }
+            //    isSuccess = false;
+            //}
         }
 
     }
 
     private void CopyToAOCFn()
     {
-        //Object[] selectedObjects = Selection.objects;
-        //server_DoorSpawner.animClips = new List<AnimationClip>();
-        //server_DoorSpawner.otherObjects = new List<Object>();
-        //AnimationClip curr;
-
-        //for (int i = 0; i < selectedObjects.Length; i++)
-        //{
-        //    server_DoorSpawner.overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>(server_DoorSpawner.animatorOController.overridesCount);
-        //    server_DoorSpawner.animatorOController.GetOverrides(server_DoorSpawner.overrides);
-        //    //Debug.Log("count = " + overrides.Count);
-        //    ////AnimationClipPair[] overrideAnimationClips = server_DoorSpawner.animatorOController.clips;
-        //    //Debug.Log("count = " + overrides.Count);
-        //    if (selectedObjects[i] is AnimationClip)
-        //    {
-        //        curr = selectedObjects[i] as AnimationClip;
-        //        server_DoorSpawner.animClips.Add(curr);
-        //        Debug.Log("curr name = " + curr.name);
-
-        //        Debug.Log("count = " + server_DoorSpawner.animatorOController.overridesCount);
-        //        Debug.Log("count = " + server_DoorSpawner.overrides.Count);
-        //        for (int r = 0; r < server_DoorSpawner.animatorOController.overridesCount; r++)
-        //        {
-        //            //Debug.Log(server_DoorSpawner.overrides[r].Key + " && " + server_DoorSpawner.overrides[r].Value);
-        //            if (server_DoorSpawner.overrides[r].Key.ToString().StartsWith(curr.name))
-        //            {
-        //                Debug.Log("g");
-        //                server_DoorSpawner.overrides[r] = new KeyValuePair<AnimationClip, AnimationClip>(server_DoorSpawner.overrides[r].Key, curr);
-        //            }
-        //        }
-
-        //        server_DoorSpawner.animatorOController.ApplyOverrides(server_DoorSpawner.overrides);
-
-        //        Debug.Log("server_DoorSpawner.animatorOController.overridesCount) " + server_DoorSpawner.animatorOController.overridesCount);
-        //    }
-        //    else
-        //    {
-        //        server_DoorSpawner.otherObjects.Add(selectedObjects[i]);
-        //        Debug.LogWarning("not selected an anim clip but its a -> " + selectedObjects[i].GetType());
-        //    }
-        //}
 
     }
 }
